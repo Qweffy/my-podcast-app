633050a333406116c3fec41f8351896b023c8be7
Qweffy <qweffooo@gmail.com>
2024-06-24 14:13:41 -0300

    chore: add and configure Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 60 * 24, // 24 horas
+            gcTime: 1000 * 60 * 60 * 24, // 24 horas
+        },
+    },
+})
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={'Home'} />
-                <Route path="/podcast/:podcastId" element={'Podcast'} />
-                <Route path="/podcast/:podcastId/episode/:episodeId" element={'PodcastEpisode'} />
-            </Routes>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={'Home'} />
+                    <Route path="/podcast/:podcastId" element={'Podcast'} />
+                    <Route
+                        path="/podcast/:podcastId/episode/:episodeId"
+                        element={'PodcastEpisode'}
+                    />
+                </Routes>
+            </Router>
+        </QueryClientProvider>
     )
 }
 
