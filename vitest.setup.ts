989bf2a5e3a122641316caa9a8f3442007d84c97
Qweffy <qweffooo@gmail.com>
2024-06-25 15:16:35 -0300

    test: add vittest config

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.test' })
+
+process.env.PODCASTS_API_URL =
+    process.env.PODCASTS_API_URL ??
+    'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
