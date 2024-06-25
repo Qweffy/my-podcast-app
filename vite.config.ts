2a386e95bac0231824aa6790c7d1174da4f6fbc1
Qweffy <qweffooo@gmail.com>
2024-06-25 14:02:04 -0300

    chore: install and configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import EnvironmentPlugin from 'vite-plugin-environment'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), EnvironmentPlugin('all')],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.ts',
+    },
 })
