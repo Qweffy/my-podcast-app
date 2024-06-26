6245ac7486fc506c9bb2c46ce709cefb2ce0ce54
Qweffy <qweffooo@gmail.com>
2024-06-25 23:49:16 -0300

    feat: mapEpisode function

--- src/api/episodies.ts
+++ src/api/episodies.ts
@@ -1,6 +1,12 @@
-import { mapEpisode, MappedEpisode, PodcastDetailsApiResponse } from 'types/Podcast'
+import { MappedEpisode, PodcastDetailsApiResponse, Episode } from 'types/Podcast'
 
 const PODCAST_EPISODES_API_URL = 'https://api.allorigins.win/get?url='
+const mapEpisode = (episode: Episode): MappedEpisode => ({
+    id: episode.trackId,
+    name: episode.trackName,
+    releaseDate: episode.releaseDate,
+    duration: episode.trackTimeMillis,
+})
 
 export const fetchPodcastEpisodes = async (podcastId: string): Promise<MappedEpisode[]> => {
     const url = `${PODCAST_EPISODES_API_URL}${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`)}`
@@ -10,6 +16,7 @@ export const fetchPodcastEpisodes = async (podcastId: string): Promise<MappedEpi
         throw new Error('Network response was not ok')
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const data: { contents: string } = await response.json()
 
     const parsedData = JSON.parse(data.contents) as PodcastDetailsApiResponse
