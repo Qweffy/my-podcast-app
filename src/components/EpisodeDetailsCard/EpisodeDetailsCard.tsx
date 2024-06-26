dea74d990327b31904a6438543165d4825201cdd
Qweffy <qweffooo@gmail.com>
2024-06-26 05:07:04 -0300

    refactor: refactor EpisodeDetails and EpisodeDetailsCard

--- src/components/EpisodeDetailsCard/EpisodeDetailsCard.tsx
+++ src/components/EpisodeDetailsCard/EpisodeDetailsCard.tsx
@@ -2,13 +2,15 @@ import { useLocation } from 'react-router-dom'
 
 export const EpisodeDetailsCard = () => {
     const location = useLocation()
-    console.log('location', location)
+    const name = location.state.name
+    const description = location.state.description
+
     return (
-        <div className="min-w-[40rem] max-w-[45rem] shadow-md border-1 p-5 flex flex-col gap-5">
-            <div>
-                <h2 className="font-bold text-xl">{episode?.name}</h2>
+        <div className="min-w-[40rem] max-w-[45rem] shadow-md border-2 border-gray-100 p-5 flex flex-col gap-5 rounded-md">
+            <div className="flex flex-col gap-2">
+                <h2 className="font-bold text-xl">{name}</h2>
                 <p className="text-sm font-semibold text-gray-500">
-                    <i>{episode?.description}</i>
+                    <i>{description || ''}</i>
                 </p>
             </div>
             <audio controls className="w-full"></audio>
--- src/pages/EpisodeDetails/EpisodeDetails.tsx
+++ src/pages/EpisodeDetails/EpisodeDetails.tsx
@@ -1,10 +1,7 @@
 import EpisodeDetailsCard from 'components/EpisodeDetailsCard'
 import PodcastDetailsCard from 'components/PodcastDetailsCard'
-import { useLocation } from 'react-router-dom'
 
 export const EpisodeDetails = () => {
-    const location = useLocation()
-    console.log('location', location)
     return (
         <div className="flex flex-col w-full items-center py-10">
             <div className="flex gap-14 items-start">
