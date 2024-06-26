1f8ca0b65f475bf7b668aa2a7c515158ecfab8b0
Qweffy <qweffooo@gmail.com>
2024-06-26 04:21:23 -0300

    refactor: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { AppRoutes } from './routes/AppRoutes.tsx'
 import queryClient from './utils/QueryClient.ts'
 import { PodcastsProvider } from 'providers/PodcastsProvider.tsx'
+import { Header } from 'components/Header/Header.tsx'
 
 export const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
             <PodcastsProvider>
+                <Header />
                 <AppRoutes />
             </PodcastsProvider>
         </QueryClientProvider>
