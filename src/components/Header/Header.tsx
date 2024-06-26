07d4e030885f4b6a91b6e3b42c4b707247b41fb8
Qweffy <qweffooo@gmail.com>
2024-06-26 05:08:24 -0300

    feat: add Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import Loader from 'components/Loader'
 
-export const Header = () => {
+export const Header = ({ isLoading }) => {
     return (
         <div className="w-full border-b-2 py-3 px-3 flex justify-between items-center text-sky-600">
-            <span className="font-semibold text-xl">Podcaster</span>
-            <Loader />
+            <Link to="/" className="font-semibold text-xl">
+                Podcaster
+            </Link>
+            {isLoading && <Loader />}
         </div>
     )
 }
