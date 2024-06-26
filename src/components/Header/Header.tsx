46b07faba5318a4c507f9f47493e907cc765d072
Qweffy <qweffooo@gmail.com>
2024-06-26 05:22:53 -0300

    refactor: update Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
 import Loader from 'components/Loader'
 
-export const Header = ({ isLoading }) => {
+export const Header = ({ isLoading }: { isLoading: boolean }) => {
     return (
-        <div className="w-full border-b-2 py-3 px-3 flex justify-between items-center text-sky-600">
-            <Link to="/" className="font-semibold text-xl">
-                Podcaster
-            </Link>
-            {isLoading && <Loader />}
+        <div className="w-full border-b-2 flex justify-center items-center text-sky-600 p-3">
+            <div className="w-3/5 flex justify-between">
+                <Link to="/" className="font-semibold text-xl">
+                    Podcaster
+                </Link>
+                {isLoading && <Loader />}
+            </div>
         </div>
     )
 }
