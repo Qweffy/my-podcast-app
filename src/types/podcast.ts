d43e8a7308bacd1ec73d4de8c64a2892643de7bd
Qweffy <qweffooo@gmail.com>
2024-06-26 03:26:53 -0300

    refactor: update types

--- src/types/podcast.ts
+++ src/types/podcast.ts
@@ -13,4 +13,36 @@ export interface Podcast {
     'im:image': {
         label: string
     }[]
+    summary: {
+        label: string
+    }
+}
+
+export interface Episode {
+    trackId: number
+    trackName: string
+    releaseDate: string
+    trackTimeMillis: number
+    description: string
+}
+
+export interface PodcastDetailsApiResponse {
+    resultCount: number
+    results: Episode[]
+}
+
+export interface MappedPodcast {
+    id: string
+    title: string
+    author: string
+    imageURL: string
+    description: string
+}
+
+export interface MappedEpisode {
+    id: number
+    name: string
+    releaseDate: string
+    duration: number
+    description: string
 }
