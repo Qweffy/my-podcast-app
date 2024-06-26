6d780ce1561ad37233d8f9e852cd3dbe5da9801d
Qweffy <qweffooo@gmail.com>
2024-06-26 03:30:23 -0300

    refactor: update approutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Home from 'pages/Home'
 import PodcastDetails from 'pages/PodcastDetails'
+import EpisodeDetails from 'pages/EpisodeDetails'
 
 export const AppRoutes = () => {
     return (
@@ -9,7 +10,7 @@ export const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/podcast/:podcastId" element={<PodcastDetails />} />
-                <Route path="/podcast/:podcastId/episode/:episodeId" element={'PodcastEpisode'} />
+                <Route path="/podcast/:podcastId/episode/:episodeId" element={<EpisodeDetails />} />
             </Routes>
         </Router>
     )
