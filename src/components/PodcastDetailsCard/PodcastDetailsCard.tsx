b2e89590c76c7caea83ddcc6ef270cdda243b2a3
Qweffy <qweffooo@gmail.com>
2024-06-26 05:08:04 -0300

    feat: add links to PodcastDetailsCard

--- src/components/PodcastDetailsCard/PodcastDetailsCard.tsx
+++ src/components/PodcastDetailsCard/PodcastDetailsCard.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { usePodcastsContext } from 'context/PodcastsContext.ts'
 
 export const PodcastDetailsCard = () => {
@@ -8,13 +8,24 @@ export const PodcastDetailsCard = () => {
     const { imageURL, title, author, description } = podcast ?? {}
 
     return (
-        <section className="flex flex-col shadow-md min-h-[10rem] border border-1 max-w-[14rem] px-3 pt-4 gap-3">
-            <img src={imageURL} className="shadow" alt={`${title} cover`} />
-            <div className="border-t-2 py-3">
-                <h3 className="text-sm font-semibold">{title}</h3>
-                <p className="text-sm">
-                    <i>{author}</i>
-                </p>
+        <section className="flex flex-col rounded-md shadow-md min-h-[10rem] border border-1 max-w-[16rem] min-w-[14rem] px-3 pt-4 gap-3">
+            <Link to={`/podcast/${podcastId}`}>
+                <img
+                    src={imageURL}
+                    className="shadow mx-auto my-2 rounded-md"
+                    alt={`${title} cover`}
+                    width="150rem"
+                />
+            </Link>
+            <div className="border-t-2 pt-3">
+                <Link to={`/podcast/${podcastId}`}>
+                    <h3 className="text-md font-semibold">{title}</h3>
+                </Link>
+                <Link to={`/podcast/${podcastId}`}>
+                    <p className="text-sm">
+                        <i>{author}</i>
+                    </p>
+                </Link>
             </div>
             <div className="border-t-2 py-3">
                 <h3 className="text-sm font-semibold">Description:</h3>
