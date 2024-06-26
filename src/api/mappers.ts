8af51877b5c86226bbdef739595f91f44244dbc9
Qweffy <qweffooo@gmail.com>
2024-06-25 23:18:44 -0300

    feat: update api calls, add episodies call

---
+++ src/api/mappers.ts
@@ -0,0 +1,8 @@
+import { MappedPodcast, Podcast } from 'types/Podcast'
+
+export const mapPodcast = (podcast: Podcast): MappedPodcast => ({
+    id: podcast.id.attributes['im:id'],
+    title: podcast['im:name'].label,
+    author: podcast['im:artist'].label,
+    imageURL: podcast['im:image'][2]?.label || '',
+})
--- src/api/podcasts.ts
+++ src/api/podcasts.ts
@@ -1,20 +1,24 @@
-import { Podcast } from 'types/Podcast.ts'
+import { mapPodcast } from './mappers'
+import { MappedPodcast, Podcast } from 'types/Podcast'
 
 interface ApiResponse {
     feed: {
         entry: Podcast[]
     }
 }
+
 const PODCASTS_API_URL = process.env.PODCASTS_API_URL
 
 if (!PODCASTS_API_URL) {
     throw new Error('PODCASTS_API_URL is not defined')
 }
 
-export const fetchPodcasts = async (): Promise<ApiResponse> => {
+export const fetchPodcasts = async (): Promise<MappedPodcast[]> => {
     const response = await fetch(PODCASTS_API_URL)
     if (!response.ok) {
         throw new Error('Network response was not ok')
     }
-    return response.json() as Promise<ApiResponse>
+
+    const data: ApiResponse = (await response.json()) as ApiResponse
+    return data.feed.entry.map(mapPodcast)
 }
